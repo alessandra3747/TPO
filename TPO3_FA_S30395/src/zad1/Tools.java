610fd7be0f651b6368eec4be6884153667ad6a4c
alessandra3747 <olafus3a@gmail.com>
2025-04-10 14:53:46 +0200

    Start implementing Tools class

--- TPO3_FA_S30395/src/zad1/Tools.java
+++ TPO3_FA_S30395/src/zad1/Tools.java
@@ -7,5 +7,20 @@
 package zad1;
 
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Tools {
+
+    static Options createOptionsFromYaml(String fileName) throws Exception {
+
+        String yaml = Files.readAllLines(Paths.get(fileName))
+                .stream()
+                .collect(Collectors.joining(System.lineSeparator()));
+
+        return null;
+    }
+
 }
